8cdcebe137627784efcb5f709971a9e9f695066a
Helberth <helberth12@gmail.com>
2025-11-13 17:54:48 -0500

    feat: initial config to run the microservice with bd

--- resources-service/src/main/java/com/lazarus/resources/ResourcesServiceApplication.java
+++ resources-service/src/main/java/com/lazarus/resources/ResourcesServiceApplication.java
@@ -2,7 +2,9 @@ package com.lazarus.resources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ResourcesServiceApplication {
 
