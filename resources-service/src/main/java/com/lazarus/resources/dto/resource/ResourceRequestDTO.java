2cb7b29fe1219d19316452e9a4dacb68a4b5369c
Helberth <helberth12@gmail.com>
2025-11-17 14:54:17 -0500

    ??? no idea, my bad

--- resources-service/src/main/java/com/lazarus/resources/dto/resource/ResourceRequestDTO.java
+++ resources-service/src/main/java/com/lazarus/resources/dto/resource/ResourceRequestDTO.java
@@ -2,8 +2,10 @@ package com.lazarus.resources.dto.resource;
 
 import com.lazarus.resources.dto.common.IdOnlyDTO;
 import java.util.Map;
+import java.util.UUID;
 
 public record ResourceRequestDTO(
+        UUID id,
         String code,
         String name,
         Map<String, Object> attributesJson,
